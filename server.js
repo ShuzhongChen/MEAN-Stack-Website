3712102d90dc1ef8ab1661cdc3538093c043873d
“ShuzhongChen” <chenshuzhongs@gmail.com>
2017-11-07 05:40:00 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT;
+
+app.set('view engine', 'jade');
+app.set('views', path.resolve(__dirname, 'client', 'views'));
+
+app.get('/', function(req, res){
+    res.render('index.jade');
+});
+
+app.listen(port, function(){
+    console.log('SERVER RUNNIDNG... PORT: ' + port);
+});

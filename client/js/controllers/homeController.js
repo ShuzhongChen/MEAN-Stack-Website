a07c38bfee368714d265eca444e428cbd9a2f903
“ShuzhongChen” <chenshuzhongs@gmail.com>
2017-11-11 01:32:36 +0000

    about page

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,15 @@
 myApp.controller('homeController', ['$scope', function($scope){
-    
+    $scope.myInterval = 4000;
+    $scope.slides = [{
+        id:0,
+        image: "img/code1.jpeg"
+    },
+    {
+        id:1,
+        image: "img/code2.jpeg"
+    },
+    {
+        id:2,
+        image: "img/code3.jpeg"
+    }]
 }]);
